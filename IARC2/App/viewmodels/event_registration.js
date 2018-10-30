e67b464a36b52cabfff02180d2cccaa922faadd0
Gil (4Z1KD) <gil4z1kd@gmail.com>
2018-10-30 22:43:44 +0200

    extended event registration form

--- IARC2/App/viewmodels/event_registration.js
+++ IARC2/App/viewmodels/event_registration.js
@@ -2,7 +2,9 @@
 
     var shell = require('viewmodels/shell');
 
+    var name = ko.observable();
     var callsign = ko.observable();
+    var email = ko.observable();
     
 
     
@@ -11,16 +13,27 @@
             if (callsign() == null) {
                 displayService.display("אל תשכח להכניס אות קריאה..", "error");
             }
+            else if (name() == null) {
+                displayService.display("אל תשכח להכניס שם..", "error");
+            }
+            else if (email() == null) {
+                displayService.display("אל תשכח להכניס אימייל..", "error");
+            }
             else {
                 var info = {
                     'info':
                     {
-                        'callsign': callsign()
+                        'name': name(),
+                        'callsign': callsign(),
+                        'email': email()
                     }
                 };
                 httpService.post("/ws/event_registration.php", info).done(function (data) {
                     displayService.display(data);
                     complete(true);
+                    callsign('');
+                    name('');
+                    email('');
                 }).error(function () { displayService.display("Something went wrong..", "error"); utilities.handleError(); complete(true); });
             }
         },
@@ -37,7 +50,9 @@
         },
         compositionComplete: function () {
         },
-        callsign: callsign        
+        name: name,
+        callsign: callsign,
+        email: email,
     };
 
 
--- IARC2/Server/event_registration.php
+++ IARC2/Server/event_registration.php
@@ -6,8 +6,8 @@ error_reporting(E_ALL);
 
 header('Content-type: application/json');
 $info = $_POST["info"];
-$sql="INSERT IGNORE INTO `event_registration` (`callsign`) VALUES ('$info[callsign]')";
-if (!mysql_query($sql))
+$sql="INSERT IGNORE INTO `event_registration` (`name`,`callsign`,`email`) VALUES ('$info[name]','$info[callsign]','$info[email]')";
+if (!mysqli_query($Link, $sql))
 {
 	echo json_encode('Error: Failed to register');
 }
@@ -15,6 +15,6 @@ else
 {
 	echo json_encode('Thank you, 73!');
 }
-mysql_close($Link);
+mysqli_close($Link);
 
 ?>

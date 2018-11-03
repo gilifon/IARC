650c382704c383b5fe675bbd7c8c44b7bb05cf07
Gil (4Z1KD) <gil4z1kd@gmail.com>
2018-11-03 05:48:32 +0200

    new version

--- IARC2/App/main.js
+++ IARC2/App/main.js
@@ -1,5 +1,5 @@
 ﻿requirejs.config({
-    urlArgs: "version=2018.4",
+    urlArgs: "version=2018.11",
     paths: {
         'text': '../Scripts/text',
         'durandal': '../Scripts/durandal',
@@ -17,7 +17,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     //>>excludeEnd("build");
 
     app.title = 'IARC - New';
-    app.version = "2018.4";
+    app.version = "2018.11";
 
     app.configurePlugins({
         router: true,

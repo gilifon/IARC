310f935549a1834758aa15490489628e18d8f3f4
Gil Lianni <gilifon@gmail.com>
2019-01-29 00:00:03 +0200

    posts

---
+++ IARC2/Server/posts.php
@@ -0,0 +1,16 @@
+<?
+include ("db_web.inc");
+include('error.inc');
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+header('Content-type: application/json');
+
+$result = mysqli_query($Link,"SELECT * FROM `posts` ORDER BY `id` DESC");
+while($obj = mysqli_fetch_object($result)) {
+$res[] = $obj;
+}
+
+header('Content-type: application/json');
+echo json_encode($res);
+?>
